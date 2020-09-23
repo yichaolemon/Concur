153268ac487b9056b9b8144117873dd05f8c02d0
yichaolemon <yclemontree0@gmail.com>
2020-09-22 21:55:45 -0700

    starter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+  println!("Hello, world!");
+  let t = create_async();
+  println!(t.value)
+}
+
+#[derive(Default, Debug)]
+struct Task<T> {
+  value: Option<T>,
+}
+
+fn create_async() -> Task<i32> {
+  let mut task = Task{
+    value: None
+  };
+  thread::spawn(move || {
+    task.value = Some(5)
+  });
+  task
 }
